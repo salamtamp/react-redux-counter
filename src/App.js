0b9d6d08ee1f7cfa6011367513482f1f692f6282
salamtamp <salamtam.p@gmail.com>
2018-11-10 23:14:30 +0700

    Add counter

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bulma/css/bulma.css';
 
+import Counter from './components/counter';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello, React !</h1>
+      <div class="container">
+        <div class="notification">
+          <h1 class="title is-1">Hello, React!</h1>
+          <Counter />
+        </div>
       </div>
     );
   }
